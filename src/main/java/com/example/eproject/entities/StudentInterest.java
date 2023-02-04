de0646679ba8d77cf16019312024ae4701d24875
DenilGEndzhev <denilgendjev@gmail.com>
2023-02-04 11:48:14 +0200

    studentInterest

--- src/main/java/com/example/eproject/entities/StudentInterest.java
+++ src/main/java/com/example/eproject/entities/StudentInterest.java
@@ -1,8 +1,13 @@
 package com.example.eproject.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class StudentInterest {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idInterest;
 }

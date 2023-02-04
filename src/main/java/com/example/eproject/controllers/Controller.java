17a799a15dd50041370daa29933c820536c5d8e2
lenovo <ntodor358@gmail.com>
2023-02-04 11:06:30 +0200

    add chooseRole(Model model)

--- src/main/java/com/example/eproject/controllers/Controller.java
+++ src/main/java/com/example/eproject/controllers/Controller.java
@@ -1,4 +1,12 @@
 package com.example.eproject.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @org.springframework.stereotype.Controller
 public class Controller {
+    @GetMapping
+    public String chooseRole(Model model){
+        return "index";
+    }
 }

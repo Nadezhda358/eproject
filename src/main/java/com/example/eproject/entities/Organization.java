05a458af0c8ee7af2ef5ed106a19ea9dbc82aa04
lenovo <ntodor358@gmail.com>
2023-02-04 10:14:16 +0200

    add Organization class

---
+++ src/main/java/com/example/eproject/entities/Organization.java
@@ -0,0 +1,13 @@
+package com.example.eproject.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+}

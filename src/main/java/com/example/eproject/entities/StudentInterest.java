dde06d9a2e7f30a81f6fdc7be4020ae08025245c
DenilGEndzhev <denilgendjev@gmail.com>
2023-02-04 13:20:53 +0200

    studentInterest 2

--- src/main/java/com/example/eproject/entities/StudentInterest.java
+++ src/main/java/com/example/eproject/entities/StudentInterest.java
@@ -4,8 +4,21 @@ import jakarta.persistence.*;
 
 @Entity
 public class StudentInterest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     @ManyToMany
     private Interest interest;
     @ManyToMany
     private Student student;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }

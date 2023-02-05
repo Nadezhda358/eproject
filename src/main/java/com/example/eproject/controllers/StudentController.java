3d1e3709fb71a579668bf99241215b9953f17bd3
lenovo <ntodor358@gmail.com>
2023-02-05 13:35:01 +0200

    change color to white

--- src/main/java/com/example/eproject/controllers/StudentController.java
+++ src/main/java/com/example/eproject/controllers/StudentController.java
@@ -39,10 +39,5 @@ public class StudentController {
         model.addAttribute("interests", interests);
         return "sign-in-student";
     }
-    @GetMapping("/all-students")
-    public String getAllStudents(Model model){
-        Iterable<Student> students = studentRepository.findAll();
-        model.addAttribute("students", students);
-        return "all-students";
-    }
+
 }
